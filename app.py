af2d1bdeafc8a054e44f8f7adcddc3895ffb2f23
Pranad Reddy <Reddy@Pranads-MacBook-Pro.local>
2018-07-29 23:33:23 -0700

    Added docstrings

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+Module to programatically extract data from all Berkeley professors from ratemyprofessors.com and store it in a database.
+Change url in load_pages() to extract professor data from other universities
+"""
+
 from selenium import webdriver
 from math import ceil
 from professor import Professor
@@ -11,13 +16,16 @@ driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=chrom
 
 
 def load_pages():
+    """
+    Loads entire page of Berkeley professors
+    """
     url = "http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+California+Berkeley&schoolID=1072&queryoption=TEACHER"
     driver.get(url)
     pages, profs_loaded = 1, 20
-    total_professors = int(driver.find_element_by_class_name("professor-count").text)
-    total_pages = ceil(total_professors / 20)
+    total_profs = int(driver.find_element_by_class_name("professor-count").text)
+    total_pages = ceil(total_profs / 20)
     print("\nLoading pages\n")
-    while profs_loaded < total_professors:
+    while profs_loaded < total_profs:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(.4)
         load_more_button = driver.find_element_by_class_name("progressbtnwrap")
@@ -30,30 +38,55 @@ def load_pages():
 
 
 def get_professor_ids():
+    """
+    Extracts professor ids from fully loaded page and returns list of them
+    Returns:
+        list: ids of all professors
+    """
     print("Extracting professor ids...\n")
     elements = driver.find_elements_by_class_name("remove-this-button")
     return [el.get_attribute("data-id") for el in elements]
 
 
-def write_professor_ids_to_file(lst):
-    print("Writing " + str(len(lst)) + " professor ids to output.txt\n")
+def write_professor_ids_to_file(prof_ids):
+    """
+    Writes list of professor ids to file
+    Args:
+        prof_ids (list): list of professor ids
+    """
+    print("Writing " + str(len(prof_ids)) + " professor ids to output.txt\n")
     with open('output.txt', 'w') as file:
-        for i in lst:
+        for i in prof_ids:
             file.write(i+"\n")
 
 
 def get_professor_ids_from_file():
+    """
+    Returns list of professor ids from file
+    Returns:
+        list: professor ids
+    """
     print("Getting professor ids from output.txt\n")
     with open('output.txt', 'r') as file:
         return [line.rstrip('\n') for line in file]
 
 
-def extract_professor_data(professor_ids):
-    num_profs = len(professor_ids)
+def extract_professor_data(prof_ids):
+    """
+    For each professor, loads page and extracts name, name_id, top 3 tags, total number of ratings,
+    top 20 (max) comments, rating, difficulty. Aggregrates data into professor object and returns list of them
+    Args:
+        prof_ids (list): professor ids
+    Returns:
+        prof_objs (list): professor objects containing their data
+    Raises:
+        Exception if certain data cannot be extracted
+    """
+    num_profs = len(prof_ids)
     print("Extracting " + str(num_profs) + " professors' data... (This will take a while)\n")
     t = 0
-    professor_objs, failed_extractions = [], []
-    for count, id in enumerate(professor_ids, 1):
+    prof_objs, failed_extractions = [], []
+    for count, id in enumerate(prof_ids, 1):
         print(str(count) + "/" + str(num_profs) + " | " + str(round(count / num_profs * 100, 2)) + "%")
         t+=1
         if t == 10:
@@ -63,8 +96,8 @@ def extract_professor_data(professor_ids):
             try:
                 name = driver.find_element_by_class_name("name").text
                 name_id = (name.split(" ")[1] + " " + name[0]).upper()
-                professor = Professor(name=name, name_id=name_id, id=id, num_ratings=0)
-                professor_objs.append(professor)
+                prof = Professor(name=name, name_id=name_id, id=id, num_ratings=0)
+                prof_objs.append(prof)
             except:
                 failed_extractions.append(id)
             continue
@@ -86,9 +119,9 @@ def extract_professor_data(professor_ids):
             difficulty = driver.find_element_by_css_selector(
                 ".breakdown-section.difficulty").find_element_by_class_name("grade").text
             tags = [e.text.split(" (")[0] for e in driver.find_elements_by_class_name('tag-box-choosetags')[0:3]]
-            professor = Professor(name=first_name + " " + last_name, name_id=name_id, id=id, num_ratings=num_ratings,
+            prof = Professor(name=first_name + " " + last_name, name_id=name_id, id=id, num_ratings=num_ratings,
                                   rating=rating, difficulty=difficulty, tags=tags, comments=comments)
-            professor_objs.append(professor)
+            prof_objs.append(prof)
         except:
             failed_extractions.append(id)
             continue
@@ -106,37 +139,49 @@ def extract_professor_data(professor_ids):
     #     print("Difficulty: ", o.difficulty)
     #     print("Tags: ", o.tags)
     #     print("Comments: ", o.comments[:3])
-    return professor_objs
+    return prof_objs
 
 
-def write_professor_objs_to_file(professor_objs):
-    print("Writing professor objects to pickled_professors.pickle\n")
-    with open("pickled_professors.pickle", "wb") as file:
-        for professor in professor_objs:
-            pickle.dump(professor,  file, pickle.HIGHEST_PROTOCOL)
+def write_prof_objs_to_file(prof_objs):
+    """
+    Pickles and writes professor objects to file
+    Args:
+        prof_objs (list): professor objects
+    """
+    print("Writing professor objects to pickled_profs.pickle\n")
+    with open("pickled_profs.pickle", "wb") as file:
+        for prof in prof_objs:
+            pickle.dump(prof,  file, pickle.HIGHEST_PROTOCOL)
 
 
 def get_professor_obs_from_file():
-    print("Getting professor objects to pickled_professors.pickle\n")
-    professor_objs = []
-    with open("pickled_professors.pickle", "rb") as file:
+    """
+    Gets, unpickles, and returns professor objects from file
+    Returns:
+        prof_objs (list): professor objects
+    """
+    print("Getting professor objects to pickled_profs.pickle\n")
+    prof_objs = []
+    with open("pickled_profs.pickle", "rb") as file:
         while True:
             try:
-                professor_objs.append(pickle.load(file))
+                prof_objs.append(pickle.load(file))
             except EOFError:
                 break
-    return professor_objs
+    return prof_objs
+
 
 if __name__ == '__main__':
     # load_pages()
-    # professor_ids = get_professor_ids()
-    # write_professor_ids_to_file(professor_ids)
-    professor_ids = get_professor_ids_from_file()
-    professor_objs = extract_professor_data(professor_ids)
-    # write_professor_objs_to_file(professor_objs)
-    # professor_objs = get_professor_obs_from_file()
+    # prof_ids = get_professor_ids()
+    # write_professor_ids_to_file(prof_ids)
+    prof_ids = get_professor_ids_from_file()
+    prof_objs = extract_professor_data(prof_ids)
+    # write_prof_objs_to_file(prof_objs)
+    # prof_objs = get_professor_obs_from_file()
     driver.quit()
 
 
 #TODO might not be 3 tags,
 #TODO at end remove reading and writing files for some methods and just have them as utility methods
+#TODO add a readme
--- professor.py
+++ professor.py
@@ -1,14 +1,33 @@
+"""
+Contains definition of Professor class
+"""
+
+
 class Professor:
+    """
+    Object to hold professor data. __init__ method sets object attributes to specified argument values
+    Args:
+        name (str): professor name
+        name_id (str): professor name in format as seen on berkeleytime.com
+        id (int): unique professor identifier on ratemyprofessors.com
+        num_ratings (int): total number of ratings
+        rating (float): rating / 5/0
+        difficulty (float): difficulty / 5
+        tags (list): top 3 tags voted to identify professor with
+        comments (list): top 20 (max) comments for professor
+    Attributes: (same as Args)
+    """
     name, name_id = '', ''
     rating, difficulty = 0.0, 0.0
     comments, tags = [], []
     id, num_ratings = 0, 0
-    def __init__(self, name, name_id, id, num_ratings, rating="N/A", difficulty="N/A", tags=None, comments=None):
+
+    def __init__(self, name, name_id, id, num_ratings, rating=-1.0, difficulty=-1.0, tags=None, comments=None):
         self.name = name
         self.name_id = name_id
         self.id = id
         self.num_ratings = num_ratings
         self.rating = rating
         self.difficulty = difficulty
-        self.tags= tags
+        self.tags = tags
         self.comments = comments
