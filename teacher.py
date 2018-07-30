9012ebe3ba2e40a12a7bf9ebcc458ae0f6d7e0a7
Pranad Reddy <Reddy@Pranads-MacBook-Pro.local>
2018-07-29 21:40:38 -0700

    Updated class def to include tags

--- teacher.py
+++ teacher.py
@@ -1,10 +1,13 @@
 class Teacher:
-    name = ''
-    rating = 0.0
-    difficulty = 0.0
-    comments = []
-    def __init__(self, name, rating, difficulty, comments):
+    name, name_id = '', ''
+    rating, difficulty = 0.0, 0.0
+    comments, tags = [], []
+    id = 0
+    def __init__(self, name, name_id, id, rating="N/A", difficulty="N/A", tags=None, comments=None):
         self.name = name
+        self.name_id = name_id
+        self.id = id
         self.rating = rating
         self.difficulty = difficulty
+        self.tags= tags
         self.comments = comments
