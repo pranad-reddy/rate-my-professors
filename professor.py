e76c71121534b71c9dd28a68e49e4a154883d9c2
Pranad Reddy <Reddy@Pranads-MacBook-Pro.local>
2018-07-29 22:09:12 -0700

    Renamed and added num_ratings attribute

--- professor.py
+++ professor.py
@@ -1,12 +1,13 @@
-class Teacher:
+class Professor:
     name, name_id = '', ''
     rating, difficulty = 0.0, 0.0
     comments, tags = [], []
-    id = 0
-    def __init__(self, name, name_id, id, rating="N/A", difficulty="N/A", tags=None, comments=None):
+    id, num_ratings = 0, 0
+    def __init__(self, name, name_id, id, num_ratings, rating="N/A", difficulty="N/A", tags=None, comments=None):
         self.name = name
         self.name_id = name_id
         self.id = id
+        self.num_ratings = num_ratings
         self.rating = rating
         self.difficulty = difficulty
         self.tags= tags
